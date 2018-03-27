7acd1e50295104e27d91d0cbf8f5f195cf78484f
derek chen <derek.g.chen@gmail.com>
2018-03-27 16:04:14 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,13 @@
-# Code your solution here!
+# # Code your solution here!
+# Guessing CLI
+#   starts the script with the run_guessing_game method (FAILED - 1)
+#   user inputs
+#     responds to 'exit' (FAILED - 2)
+#     responds to a correct guess (FAILED - 3)
+#     responds to an incorrect guess (FAILED - 4)
+
+
+def run_guessing_game 
+
+
+end
