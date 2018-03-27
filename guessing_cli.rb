7ef17842111d0d5edf5bf7f8619c64cde71fb491
derek chen <derek.g.chen@gmail.com>
2018-03-27 16:05:24 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -7,7 +7,10 @@
 #     responds to an incorrect guess (FAILED - 4)
 
 
-def run_guessing_game 
+def run_guessing_game
+  puts 'Guess a number between 1 and 6.'
+  puts 'You guessed the correct number!'
 
+  puts 'Goodbye!'
 
 end
